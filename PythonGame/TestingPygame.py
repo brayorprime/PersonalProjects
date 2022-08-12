49aaaf430a1b0e03e2af1704618a773faa971632
Clarimount <32919394+brayorprime@users.noreply.github.com>
2022-08-11 19:22:06 -0700

    Started making something

--- PythonGame/TestingPygame.py
+++ PythonGame/TestingPygame.py
@@ -4,33 +4,66 @@ import sys
 
 pygame.init()
 
-size = width, height = 620, 440
-speed = [5, 2]
-black = 0,0,0
-
+size = width, height = 1100,1100
+speed = [10, 15]
+black = 250,250,250
+#--------------------------------------
 screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("intro_ball.gif")
+background = pygame.image.load("PythonGame\Sprites\\background.png")
+ball = pygame.image.load("PythonGame\Sprites\lilclari.png")
 
-ballrect = ball.get_rect()
+position = ball.get_rect()
 
+bgrect = background.get_rect()
 
-while 1:
+screen.blit(background, (0,0))
+pygame.display.update()
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
+class GameObject:
+    def __init__(self, image, height, speed):
+        self.speed = speed
+        self.image = image
+        self.pos = image.get_rect().move(0, height)
 
-	ballrect = ballrect.move(speed)
-	if ballrect.left < 0 or ballrect.right > width:
+    def move(self):
+        self.pos = self.pos.move(0, self.speed)
+        if self.pos.right > 600:
+            self.pos.left = 0
+
+def exitGame():
+    for event in pygame.event.get():
+        if event.type in (QUIT, KEYDOWN):
+            sys.exit()
+
+while True:
+	screen.blit(background, position, position)
+	
+	exitGame()
+
+	position = position.move(speed)
+	
+	if position.left < 0 or position.right > bgrect.right:
 		speed[0] = -speed[0]
 
-	if ballrect.top < 0 or ballrect.bottom > height:
+	if position.top < 0 or position.bottom > bgrect.bottom:
 		speed[1] = -speed[1]
 
-	screen.fill(black)
-	screen.blit(ball, ballrect)
+	screen.blit(ball, position)
+	pygame.display.update()
 	pygame.display.flip()
+	pygame.time.delay(10)
+#--------------------------------------
 
+# o = GameObject(player, x*40, x*10)
 
+# while True:
+	
+# 	screen.blit(background, o.pos, o.pos)
 
+# 	o.move()
+# 	screen.blit(o.image, o.pos)
+
+# 	pygame.display.update()
+
+# 	pygame.time.delay(100)
